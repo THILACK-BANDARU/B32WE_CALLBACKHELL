b84b236d0add33559bef57d6a9d71bd40ebaa8ac
THILACK-BANDARU <82490425+THILACK-BANDARU@users.noreply.github.com>
2022-03-27 12:36:32 +0530

    Call backhell task upload

---
+++ callbackhell.js
@@ -0,0 +1,33 @@
+function hell(){
+    document.getElementById('counter').innerHTML="10";
+    setTimeout(function(){
+        document.getElementById('counter').innerHTML="9"
+        setTimeout(function(){
+            document.getElementById('counter').innerHTML="8"
+            setTimeout(function(){
+                document.getElementById('counter').innerHTML="7"
+                setTimeout(function(){
+                    document.getElementById('counter').innerHTML="6"
+                    setTimeout(function(){
+                        document.getElementById('counter').innerHTML="5"
+                        setTimeout(function(){
+                            document.getElementById('counter').innerHTML="4"
+                            setTimeout(function(){
+                                document.getElementById('counter').innerHTML="3"
+                                setTimeout(function(){
+                                    document.getElementById('counter').innerHTML="2"
+                                    setTimeout(function(){
+                                        document.getElementById('counter').innerHTML="1"
+                                        setTimeout(function(){
+                                            document.getElementById('counter').innerHTML="HAPPY INDEPENDENCE DAY"},1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+}
+hell();
